1f4ee0d8953e421c8c47dfa6c75fc41c9db66591
Billy <billyn0213@gmail.com>
2022-07-30 14:15:47 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+$(() => {
+$('h1').on('mouseover', () => {
+    $('#hello').show(2000)
+})
+
+
+
+
+    let currentImgIndex = 0
+    let numOfImages = $('.container').children().length - 1
+    
+    
+    
+    $('.next').on('click', () => {
+        $('.container').children().eq(currentImgIndex).
+        css('display', 'none')
+        ///////////////////////adding an event handler for the "next" button//////////////////////////
+        if(currentImgIndex < numOfImages) {
+            currentImgIndex++
+            } else {
+            currentImgIndex = 0
+            }
+            $('.container').children().eq(currentImgIndex).
+            css('display', 'block')
+    })
+    
+    
+    // don't forget to put a period on "previouse" because that's a class 
+    $('.previous').on('click', () => {
+    //////////////////////adding event handler for the "previous" button/////////////////////////////
+        $('.container').children().eq(currentImgIndex).css('display', 'none')
+        
+        
+        if(currentImgIndex > 0) {
+            currentImgIndex--
+        } else {
+            currentImgIndex = numOfImages
+        }
+        $('.container').children().eq(currentImgIndex).css('display', 'block')
+    })
+    
+    
+    /////////////////////// This is a simple carousel that you can use for jquery /////////////////////
+    
+    
+    
+    
+    
+    
+    });
+    
