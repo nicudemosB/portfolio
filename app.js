a995ca14c38a1cb4338a34b503a0f459ec9ad38e
Billy <billyn0213@gmail.com>
2022-08-01 15:44:47 -0400

    made a modal for projects

--- app.js
+++ app.js
@@ -78,3 +78,19 @@ $('h2').on('click', () => {
         }
     }
     
+    let modal = document.getElementById('myModal')
+    let btn = document.getElementById('myBtn')
+    let span = document.getElementsByClassName('close')[0]
+    btn.onclick = function() {
+        modal.style.display = 'block'
+    }
+
+    span.onclick = function() {
+        modal.style.display ='none'
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = 'none'
+        }
+    }
